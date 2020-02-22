9a7c33f492f9937e4d10309fea9f6171b24b5c93
Jason Hall <jdhall75@gmail.com>
2020-02-22 09:53:23 -0500

    test and clone theme into blog repo

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+THEME_DIR=flex
+THEME_REPO='git@github.com:jdhall75/Flex.git'
+
+if [ -d $THEME_DIR ]; then
+    rm -fr $THEME_DIR
+fi
+
+git clone $THEME_REPO $THEME_DIR
+make clean
+pelican -t flex
